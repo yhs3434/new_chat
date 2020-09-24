1e3db0287c0789014e6d5b019777843f5356119a
Yoon Han Sol <yoonhs3434@naver.com>
2020-09-24 18:06:22 +0900

    클라 -> 서버로의 채팅은 만들음

--- chat_server.js
+++ chat_server.js
@@ -1,12 +1,51 @@
 // 노드로 실행할 것.
 
+const wsLib = require('./lib/websocket');
+const decodeFromJs = wsLib.decodeFromJs;
+
 const WebSocket = require('ws');
 const wss = new WebSocket.Server({port:8081});
 
+let users = new Map();
+
 wss.on('connection', (ws) => {
     //console.log(ws);
-    ws.on('message', (message) => {
-        console.log(`received : ${message}`);
+    ws.on('message', (req) => {
+        const message = decodeFromJs(req);
+        // console.log(`received : ${message}`);
+        const {type, payload} = message;
+        if (type === 'init') {
+            const {clientId} = payload;
+            console.log(`${clientId} is connected`);
+
+            const payload = {
+                clientId : clientId,
+                groupId : users[clientId]
+            };
+            const res = {
+                type : 'init',
+                payload : payload
+            };
+            ws.send(res);
+        } else if (type === 'join') {
+            const {clientId, groupId} = payload;
+
+            if (typeof(users[clientId]) === 'undefined') {
+                users[clientId] = new Array();
+            }
+            users[clientId].push(groupId);
+            
+        } else if (type === 'withdraw') {
+            const {clientId, groupId} = payload;
+
+            const idx = users[clientId].indexOf(groupId);
+            if (idx > -1) {
+                users[clientId].splice(idx, 1);
+            }
+        } else if (type === 'message') {
+            const {msg} = payload;
+            console.log(`message : ${msg}`);
+        }
     });
 
     ws.send('something');
--- client/sendMessage.js
+++ client/sendMessage.js
@@ -1,5 +1,9 @@
 // 메세지 전송 js
 
+function encodeToJs(message) {
+    return escape(JSON.stringify(message));
+}
+
 const host = "localhost";
 const port = 8081;
 const hostPort = `ws://${host}:${port}`;
@@ -8,5 +12,32 @@ console.log(hostPort);
 
 const ws = new WebSocket(hostPort);
 ws.onopen = (evt) => {
-    ws.send('test');
+    ws.send(encodeToJs(
+        {
+            type: 'start',
+            payload: {
+                message: 'test'
+            }
+        }
+    ));
 }
+
+function buttonClick() {
+    const elem = document.getElementById('inputMessage');
+    const value = elem.value;
+    elem.value = '';
+    const data = {
+        type: 'message',
+        payload: {
+            msg: value
+        }
+    };
+    ws.send(encodeToJs(data));
+};
+
+document.getElementById('btnSend').addEventListener("click", buttonClick);
+document.getElementById('inputMessage').addEventListener("keyup", (evt) => {
+    if (window.event.keyCode === 13) {
+        buttonClick();
+    }
+});
